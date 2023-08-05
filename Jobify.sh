e27a1f3af12eb6c4e97f780fa4d271794d96d8a2
Humphrey <h.nyahoja@alustudent.com>
2023-08-05 11:11:07 -0700

    edited Jobify.sh and put 4 options for the user to choose from at the sign-in page

--- Jobify.sh
+++ Jobify.sh
@@ -2,3 +2,14 @@
 echo "============================================WELCOME TO NEGPOD 10 PLD PROJECT==========================================="
 sleep 1
 echo "=====================================================JOBIFY============================================================"
+sleep 1
+echo "-----ENTER 1 TO CREATE AN ACCOUNT-----"
+sleep 1
+echo "-----ENTER 2 TO LOGIN-----"
+sleep 1
+echo "-----ENTER 3 TO RECOVER PASSWORD-----"
+sleep 1
+echo "-----ENTER 4 TO EXIT-----"
+sleep 1
+echo "ENTER VALID OPTION:"
+read -r OPTION
